bb1cd0105c1b24f421c44f3d0bc9ba9a65f00e62
Menaka Nilupul <168346585+miztaxx@users.noreply.github.com>
2025-08-22 07:09:47 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// Config.js - Bot Configuration
+// You can change the basic settings for your bot here.
+
+const Config = {
+    botName: "Night Wa Bot - Open source Project", // Bot's name
+    prefix: ".", // The prefix for commands (e.g., .ping)
+    ownerNumber: "94774533771", // The owner's number
+    sessionName: "session" // The name for the session file
+};
+
+export default Config;
