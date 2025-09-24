b3df9244820b3372406362ad2c18eaeec48759da
chamodmihiranga <cmihiranga200@gmail.com>
2025-09-24 17:44:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 const Config = {
     botName: "Night Wa Bot - Open source Project", // Bot's name
     prefix: ".", // The default prefix for commands (can be changed via .settings)
-    ownerNumber: "94774533771", // The owner's number without '+' or '@s.whatsapp.net'
-    sessionName: "session" // The name for the session file
+    ownerNumber: "94761480834", // The owner's number without '+' or '@s.whatsapp.net'
+    sessionName: "24121061942cff1a" // The name for the session file
 };
 
 export default Config;
